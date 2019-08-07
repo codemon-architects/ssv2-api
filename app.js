f132e22a45e83d99cfe67ac2fe8638133294e54a
Andrej Rasevic <andrej@rasevicengineering.com>
2019-08-07 15:01:08 -0400

    basic cas auth implemented

--- app.js
+++ app.js
@@ -2,19 +2,47 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var passport = require('passport')
 var app = express();
-
+var session = require('express-session');
+var casLogin = require('./middleware/casLogin');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('super secret'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use( session({
+  secret            : 'supersecret',
+  resave            : false,
+  saveUninitialized : true
+}));
+
+app.get('/test', function(req, res) {    
+  res.send('Welcome to Passport with CAS!!!');
+});
+
+//var serverBaseURL = app.get('http') + app.get('host') + ':' + app.get('port')+'/';
+//const serverBaseURL = 'https://shib.idm.umd.edu/shibboleth-idp/profile/cas/';
+const serverBaseURL = 'http://localhost:3000/'
+  
+
+const cas = new (require('passport-cas').Strategy)({
+  ssoBaseURL: 'https://shib.idm.umd.edu/shibboleth-idp/profile/cas/',
+  serverBaseURL,
+  validate: '/serviceValidate'
+}, function(login, cb) {
+  // all we get from CAS validation is the directoryID, which is all we need
+  if (login != null) {
+    console.log(login);
+    cb(null,login);    
+  } else {
+    cb(null, false, { message: "cannot decode CAS response" })
+  }
+})
+passport.use(cas);
+
+app.use('/cas_login', casLogin({app: app}));
 
 module.exports = app;
---
+++ middleware/casLogin.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+
+module.exports = function(options = {}) {
+  const app = options.app;
+  console.log('app:')
+  console.log(app)
+  //const frontend = app.get('frontend');
+  const frontend = 'http://localhost:3000/test';
+  const errorRedirectURL = `${frontend}?invalid`;
+  //const { cookie: cookieParams } = app.get('authentication');
+const cookieParams = {
+  "enabled": true,
+      "name": "express-jwt",
+      "httpOnly": false,
+      "secure": false,
+      "maxAge": 86400000
+}
+
+  return function casLogin(req, res, next) {
+    passport.authenticate('cas',function(err, user, info) {
+      if (err) {
+        // login error
+        res.redirect(errorRedirectURL);
+      } 
+      
+      // user authenticated
+      res.redirect(frontend)
+      //res.send("Logged in!")
+      //return app.passport.createJWT({ userId: 'populateWithAUserId' }, app.get('authentication')).then(accessToken => {
+        // have to do this manually for feathers-authentication-client to accept the jwt
+        // default maxAge is 86400000 or 1 day in MS
+        //console.log('will this get reached?')
+        //res.cookie(cookieParams.name, accessToken, { maxAge: cookieParams.maxAge, httpOnly: !!cookieParams.httpOnly });
+        //res.redirect(frontend);
+     // });
+    })(req, res, next);
+  };
+};
